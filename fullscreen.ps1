7f883ba8a91e5872182427d05fccbe72dc71f1f6
LegendarySir <simonraff@gmx.de>
2026-01-14 21:51:13 +0100

    Update fullscreen.ps1

--- fullscreen.ps1
+++ fullscreen.ps1
@@ -1,1 +1,13 @@
-Add-Type -AssemblyName System.Windows.Forms,System.Drawing;[System.Windows.Forms.Screen]::AllScreens | %{$f=New-Object System.Windows.Forms.Form;$f.FormBorderStyle=0;$f.StartPosition=0;$f.Bounds=$_.Bounds;$f.BackColor=[System.Drawing.Color]::Red;$f.TopMost=$true;$f.Show()};[System.Windows.Forms.Application]::Run()
+Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+
+[System.Windows.Forms.Screen]::AllScreens | ForEach-Object {
+    $f = New-Object System.Windows.Forms.Form
+    $f.FormBorderStyle = 'None'
+    $f.StartPosition  = 'Manual'
+    $f.Bounds         = $_.Bounds
+    $f.BackColor      = [System.Drawing.Color]::Red
+    $f.TopMost        = $true
+    $f.Show()
+}
+
+[System.Windows.Forms.Application]::Run()
